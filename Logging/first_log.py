a29140bc9efc2d37842d99bd6684510590e924b7
Adam Steventon <officialadamsteventon@gmail.com>
2021-11-04 15:02:15 +0000

    my logging calculator

---
+++ Logging/first_log.py
@@ -0,0 +1,36 @@
+import logging
+
+# print(dir(logging))
+
+logging.basicConfig(filename='test.log', format='%(asctime)s : %(levelname)s : 	%(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+
+def add(x, y):
+    '''Add Function'''
+    return x + y
+
+def subtract(x, y):
+    '''Add Function'''
+    return x - y
+
+def multiply(x, y):
+    '''Add Function'''
+    return x * y
+
+def divide(x, y):
+    '''Add Function'''
+    return x // y
+
+num_1 = 100
+num_2 = 20
+
+add_result = add(num_1, num_2)
+logging.info(f'Add {num_1} + {num_2} = {add_result}')
+
+sub_result = subtract(num_1, num_2)
+logging.info(f'Subtract {num_1} - {num_2} = {sub_result}')
+
+multiply_result = multiply(num_1, num_2)
+logging.info(f'Multiply {num_1} * {num_2} = {multiply_result}')
+
+divide_result = divide(num_1, num_2)
+logging.info(f'Divide {num_1} / {num_2} = {divide_result}')
